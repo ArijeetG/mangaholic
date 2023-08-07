d83183f1e0236c2c82f2ed4e846245b511b228c2
ArijeetG <arijeetghosh251@gmail.com>
2023-08-08 01:53:52 +0530

    initial commit

---
+++ screens/Home.js
@@ -0,0 +1,45 @@
+import { Text, SafeAreaView } from "react-native";
+import React from "react";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Latest"
+        component={Latest}
+        options={{
+          tabBarIcon: () => <Icon name="home" size={22} />,
+          tabBarLabel: "",
+        }}
+      />
+      <Tab.Screen
+        name="Library"
+        component={Library}
+        options={{
+          tabBarIcon: () => <Icon name="book" size={22} />,
+          tabBarLabel: "",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function Latest() {
+  return (
+    <SafeAreaView>
+      <Text>Latest</Text>
+    </SafeAreaView>
+  );
+}
+
+function Library() {
+  return (
+    <SafeAreaView>
+      <Text>Library</Text>
+    </SafeAreaView>
+  );
+}
